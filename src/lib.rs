c98c357a1c992f8ab7dc4a84632fbc4a13a21bbb
Felix Schütt <felix.schuett@maps4print.com>
2019-01-08 03:43:33 +0100

    Removed test data from libflate, removed unnecessary num-traits
    Added azul features, only logging enabled by default

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,36 @@
+#[cfg_attr(feature = "svg", macro_use)]
 pub extern crate glium;
 pub extern crate gleam;
+
+pub extern crate lazy_static;
 pub extern crate euclid;
+pub extern crate webrender;
 pub extern crate rusttype;
 pub extern crate app_units;
 pub extern crate unicode_normalization;
-pub extern crate lazy_static;
 pub extern crate tinyfiledialogs;
 pub extern crate clipboard2;
-pub extern crate serde;
+pub extern crate font_loader;
+
+#[cfg(feature = "logging")]
 pub extern crate log;
+#[cfg(feature = "svg")]
 pub extern crate stb_truetype;
-// pub extern crate font_loader;
+#[cfg(feature = "logging")]
 pub extern crate fern;
+#[cfg(feature = "logging")]
 pub extern crate backtrace;
-pub extern crate webrender;
+#[cfg(feature = "image_loading")]
+pub extern crate image;
+#[cfg(feature = "serde_serialization")]
+#[cfg_attr(feature = "serde_serialization", macro_use)]
+pub extern crate serde;
+#[cfg(feature = "svg")]
+pub extern crate lyon;
+#[cfg(feature = "svg_parsing")]
+pub extern crate usvg;
+#[cfg(feature = "faster-hashing")]
+pub extern crate twox_hash;
+
+// #[cfg(not(target_os = "linux"))]
+// extern crate nfd;
